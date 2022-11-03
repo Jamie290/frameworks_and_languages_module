a1d290da17cb974e7253195ba88192dcfe64fe11
Jamie290 <jamie.bah29@gmail.com>
2022-11-03 17:34:48 +0000

    First Post endpoint First Items GET endpoint

--- server/app.js
+++ server/app.js
@@ -2,15 +2,29 @@ const express = require('express')
 const app = express()
 const port = 8000
 
+app.use(express.json());
+
+ITEMS = []
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/thing', (req, res) => {
   res.json({ user: 'jamie' })
-})
+}) 
 
-//app.post('', ) (req, res) => { })
+app.get('/items/', (req, res) => {
+  res.json(ITEMS)
+}) 
+
+
+app.post('/item/', (req, res) => {
+  //console.log('POST POST POST!!!')
+  //console.log(req.body)
+  ITEMS.push(req.body)
+  res.json({})
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
