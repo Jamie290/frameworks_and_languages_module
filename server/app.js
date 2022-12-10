8936ba421d5ee99c00fc14e7db4c20dbfa92ccac
Jamie290 <jamie.bah29@gmail.com>
2022-12-10 19:58:22 +0000

    added cors headers and filter username

--- server/app.js
+++ server/app.js
@@ -9,8 +9,17 @@ app.use(bodyParser.urlencoded({ extended: true })) //allows specific access to n
 app.use(cors())
 app.use(express.json());
 
+app.use((req, res, next) => { //ensures we prevent cors errors
+res.header('Access-Control-Allow-Origin', '*'); // allows the response from one website to a request originating from another website, and identifies the permitted origin of the request
+res.header('Access-Control-Allow-Headers', '*'); //defines which type headers we want to accept along with request
+if(req.method == 'OPTIONS') {
+  res.header('Access-Control-Allow-Methods', 'POST','GET','OPTIONS','DELETE'); //Make sure my methods are all the HTTP requests i want to run with the API
+  return res.status(204).json({});
+}
+})
+
 var ITEMS = {
-  1: {
+  0: {
       "id": 1,
       "user_id": "user1234",
       "keywords": ["hammer", "nails", "tools"],
@@ -23,15 +32,15 @@ var ITEMS = {
 
 
 app.post('/item', (req,res) => {
-  var id = Object.keys(ITEMS).length  + 1
+  var idNew = Object.keys(ITEMS).length  + 1
   var date = new Date().toJSON().slice(0,10)
-  if(ITEMS.hasOwnProperty(id)){
-    id = id + 1
+  if(ITEMS.hasOwnProperty(idNew)){
+    idNew = idNew + 1
   }
   if(req.body.user_id && req.body.keywords && req.body.description 
     && req.body.lat && req.body.lon !== ""){
-    ITEMS[id] = {
-      id: id,
+    ITEMS[idNew] = {
+      id: idNew,
       user_id: req.body.user_id,
       keywords: req.body.keywords,
       description: req.body.description,
@@ -41,7 +50,7 @@ app.post('/item', (req,res) => {
       date_from: date ,
       date_to: date }
 
-      res.status(201).json(ITEMS[id]) 
+      res.status(201).json(ITEMS[idNew]) 
   }
       else {
         res.status(405).json('Missing field')
@@ -55,16 +64,25 @@ app.get('/', (req, res) => {
   return res.status(200).send('<html><body>Your HTML text</body></html>')
 })
 
-
+app.get('/items' ,(req,res)=>{
+  if(req.query.user_id)
+  {
+    res.status(200).json(Object.values(ITEMS).filter(i  => i.user_id == req.query.user_id))
+    return;
+  }
+  res.status(200).json(Object.values(ITEMS))
+})
 
 app.get('/items', (req, res) => {
-  res.status(200).json(ITEMS[id])
+  res.status(200).json(ITEMS[idNew])
 }) 
 
 app.delete('/item/:id', (req,res) => { 
-  var id = parseInt(req.params.id)
-  if(ITEMS.hasOwnProperty(id)){
-    delete ITEMS[id]
+  
+
+  var idNew = parseInt(req.params.idNew)
+  if(ITEMS.hasOwnProperty(idNew)){
+    delete ITEMS[idNew]
     res.status(204).send("Ok")
   }
   else{
@@ -82,3 +100,6 @@ app.listen(port, () => {
 })
 // Docker container exit handler - https://github.com/nodejs/node/issues/4182
 process.on('SIGINT', function() {process.exit()})
+
+//References
+//https://www.youtube.com/watch?v=zoSJ3bNGPp0 "(Handling CORS errors and body parsing)"
